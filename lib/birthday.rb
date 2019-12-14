9c9246aa29404724d455ab065f23852ef653716a
Marcel Rico <personal.mr4@gmail.com>
2019-12-14 17:34:09 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,5 +11,10 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |name,age|
+    bday_greeting = "Happy Birthday #{name}! You are now #{age} years old!"
+    puts bday_greeting if age <= 12
+  end
+end
 
